302b495fd0e529aebe7e73f5ce2004803508c4a1
Jorge Leyva <jassan851031@gmail.com>
2020-05-28 01:38:27 -0400

    Modal Insert, button trash, filter done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Agregar from './components/agregar'
+import Agregar from './components/agregar';
 
 
 
@@ -9,23 +9,51 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: []
+      users: [],
+      filter: ''
     }
   };
 
-  
 
+  addUser = (user) => {
+    this.setState({
+      users: [...this.state.users, user]
+    });
+  }
+
+  handleChange = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  trash(y) {
+    const { users } = this.state;
+    users.splice(y, 1);
+    this.setState({
+      users: users
+    })
+  }
 
   render() {
+
+    const { filter, users } = this.state;
+    const lowercasedFilter = filter.toLowerCase();
+    const filteredData = users.filter(item => {
+      return Object.keys(item).some(key =>
+        item[key].toLowerCase().includes(lowercasedFilter)
+      );
+    });
     return (
       <>
-        <Agregar />
+        <Agregar addUser={this.addUser} />
         <div className="container d-flex justify-content-center">
           <div className="row">
             <div className="table-responsive-md">
-              <button type="button" className="btn btn-primary mb-1" data-toggle="modal" data-target="#exampleModal">
-                Insert
-            </button>
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" id="inputGroup-sizing-default">Filter</span>
+                </div>
+                <input id="inputFilter" onChange={this.handleChange} type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+              </div>
               <table className="table table-lg table-dark">
                 <thead>
                   <tr>
@@ -34,20 +62,25 @@ class App extends React.Component {
                     <th scope="col">Lastname</th>
                     <th scope="col">Age</th>
                     <th scope="col">RUT</th>
+                    <th scope="col"><button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                      Insert
+                    </button></th>
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>
-                      <button className="btn btn-dark btn-block border-white" data-toggle="modal">Modify</button>
-                    </td>
-                    <td><button className="btn btn-danger btn-block" data-toggle="modal" data-target="#staticBackdrop"><i className="fa fa-trash"></i></button></td>
-                  </tr>
+                  {
+                    filteredData.map((user, i) => (
+                      <tr key={i}>
+                        <td> {i + 1}</td>
+                        <td> {user.nombre}</td>
+                        <td> {user.apellido}</td>
+                        <td> {user.edad}</td>
+                        <td> {user.rut}</td>
+                        <td><button className="btn btn-dark btn-block border-white" data-toggle="modal">Modify</button></td>
+                        <td><button className="btn btn-danger btn-block" data-toggle="modal" data-target="#staticBackdrop" onClick={y => this.trash(i)}><i className="fa fa-trash"></i></button></td>
+                      </tr>
+                    ))
+                  }
                 </tbody>
               </table>
             </div>
--- src/components/agregar.js
+++ src/components/agregar.js
@@ -2,11 +2,59 @@ import React from 'react'
 
 class Agregar extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+                nombre: '',
+                apellido: '',
+                edad: '',
+                rut: ''
+        };
+    }
+
+    nombreHandleChange = event => {
+        this.setState({
+            nombre: event.target.value
+        })
+    }
+
+    apellidoHandleChange = event => {
+        this.setState({
+            apellido: event.target.value
+        })
+    }
+
+    edadHandleChange = event => {
+        this.setState({
+            edad: event.target.value
+        })
+    }
+
+    rutHandleChange = event => {
+        this.setState({
+            rut: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.props.addUser(this.state);
+        this.setState(
+            {
+                nombre: '',
+                apellido: '',
+                edad: '',
+                rut: ''
+            }
+        )
+      }
+
     render() {
         return (
             <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div className="modal-dialog">
                     <div className="modal-content">
+                        <form onSubmit={this.handleSubmit}>
                         <div className="modal-header">
                             <h5 className="modal-title" id="exampleModalLabel">New Member</h5>
                             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
@@ -17,24 +65,25 @@ class Agregar extends React.Component {
                             <div className="container">
                                 <div className="row">
                                     <div className="col-md-6">
-                                        <label for="exampleInputEmail1">Name</label>
-                                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                                        <label for="exampleInputPassword1">Age</label>
-                                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                                        <label>Name</label>
+                                        <input type="text" className="form-control" id="name" value={this.state.nombre} onChange={this.nombreHandleChange} />
+                                        <label>Age</label>
+                                        <input type="text" className="form-control" id="age" value={this.state.edad}  onChange={this.edadHandleChange} />
                                     </div>
                                     <div className="col-md-6">
-                                        <label for="exampleInputEmail1">Lastname</label>
-                                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                                        <label for="exampleInputPassword1">RUT</label>
-                                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                                        <label>Lastname</label>
+                                        <input type="text" className="form-control" id="apellido" value={this.state.apellido}  onChange={this.apellidoHandleChange} />
+                                        <label>RUT</label>
+                                        <input type="text" className="form-control" id="rut" value={this.state.rut}  onChange={this.rutHandleChange}/>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary">Save changes</button>
+                            <button type="submit" className="btn btn-primary">Save changes</button>
                         </div>
+                        </form>
                     </div>
                 </div>
             </div>
